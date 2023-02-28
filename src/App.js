f27d3ed6715db3aa052577356004648a79f236e0
Bryan McKernon <bryanmckernon@Bryans-MacBook-Pro.local>
2023-02-28 12:13:18 -0800

    Prior to removing google analytics React approach

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import ReactGA from 'react-ga';
+import React, { useEffect, useState } from "react";
+import ReactGA from "react-ga";
 import useAnalyticsEventTracker from "./useAnalyticsEventTracker";
 import TheClock from "./TheClock/TheClock";
 import ControlBar from "./ControlBar/ControlBar";
@@ -12,7 +12,7 @@ function App() {
   const TRACKING_ID = "G-STPKKR5ZJ7";
   const EventTrackerGA = useAnalyticsEventTracker("clock");
   ReactGA.initialize(TRACKING_ID);
-  
+
   const UpdateClockStyle = (newClockStyle) => {
     setClockStyle(newClockStyle);
   };
@@ -22,6 +22,17 @@ function App() {
     else setControlBarView(true);
     EventTrackerGA("clock-click");
   };
+
+  useEffect(() => {
+    fetch("https://www.googletagmanager.com/gtag/js?id=G-STPKKR5ZJ7");
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      // eslint-disable-next-line no-undef
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-STPKKR5ZJ7");
+  }, []);
 
   return (
     <div className="App">
